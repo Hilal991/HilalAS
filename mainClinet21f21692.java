eee50b9d75ebc1d2c38de3aba74ca378eeb801d5
Hilal991 <135147610+Hilal991@users.noreply.github.com>
2023-05-31 19:49:01 +0400

    Create mainClinet21f21692.java

---
+++ mainClinet21f21692.java
@@ -0,0 +1,11 @@
+public class mainClinet21f21692 
+{
+    public  static void main(String args[])
+    {
+        Client21f21692 Hilalcl=new Client21f21692();
+        Thread Hilalt=new Thread(Hilalcl);
+        Hilalt.start();
+        
+    }
+            
+}
