bc434c6ae0d986bd628770fe7eb4a533f0f31368
Hilal991 <135147610+Hilal991@users.noreply.github.com>
2023-05-31 19:53:55 +0400

    Create mainServer21f21692.java

---
+++ mainServer21f21692.java
@@ -0,0 +1,14 @@
+public class mainServer21f21692 
+{
+    public  static void main(String args[])
+    {
+       
+        Server21f21692 HilalSer=new Server21f21692();
+        
+        Thread Hilalt=new Thread(HilalSer);
+        Hilalt.start();
+        
+    }
+ 
+    
+}
